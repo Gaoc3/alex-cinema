9dd77984d8d474a718f22e12832e7dd553cdc747
Gaoc3 <secon2636@gmail.com>
2026-06-10 22:20:18 +0300

    Auto-commit: 2026-06-10 22:20:17

--- arabseed-premium/web/next.config.js
+++ arabseed-premium/web/next.config.js
@@ -1,1 +1,24 @@
-module.exports = { outputFileTracingRoot: __dirname }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  outputFileTracingRoot: __dirname,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+      {
+        protocol: 'http',
+        hostname: '**',
+      }
+    ],
+    localPatterns: [
+      {
+        pathname: '/api/proxy',
+        search: '',
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
