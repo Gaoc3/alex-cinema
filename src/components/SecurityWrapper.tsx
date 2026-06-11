2e1b360259746fa272d3929af4333e7c2401cad5
Gaoc3 <secon2636@gmail.com>
2026-06-12 01:04:20 +0300

    chore: restore SecurityWrapper protection

--- src/components/SecurityWrapper.tsx
+++ src/components/SecurityWrapper.tsx
@@ -32,9 +32,25 @@ export default function SecurityWrapper({ children }: { children: React.ReactNod
     document.addEventListener('contextmenu', handleContextMenu);
     document.addEventListener('keydown', handleKeyDown);
 
+    // 3. Debugger Loop Trap
+    // This will freeze the browser if DevTools is open.
+    // We run it every second to annoy any open DevTools.
+    const debuggerTrap = setInterval(() => {
+      const start = new Date().getTime();
+      // eslint-disable-next-line no-debugger
+      debugger;
+      const end = new Date().getTime();
+      // If it took more than 100ms, it means the debugger actually paused execution.
+      if (end - start > 100) {
+        // We could theoretically redirect or clear the page here
+        // document.body.innerHTML = 'Nice try!';
+      }
+    }, 1000);
+
     return () => {
       document.removeEventListener('contextmenu', handleContextMenu);
       document.removeEventListener('keydown', handleKeyDown);
+      clearInterval(debuggerTrap);
     };
   }, []);
 
