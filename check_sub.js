0725b26c000a38f33157d2ee0a8fc799daef98d0
Gaoc3 <secon2636@gmail.com>
2026-06-13 19:28:14 +0300

    Auto-commit: 2026-06-13 19:28:14

--- check_sub.js
+++ check_sub.js
@@ -1,5 +1,5 @@
 async function test() {
-  const url = 'http://cnth2.shabakaty.com/vascin-translation-files/DD0974DF-6854-9677-74C8-5CB1C30AEB90_ar_transfile.srt?response-cache-control=max-age%3D86400&AWSAccessKeyId=PSFBSAZRKNBJOAMKHHBIBOBEONKBBOPKEDDBFBOJCH&Expires=1783958715&Signature=wSHpLyQbtuG31Nkd8u0wqKGEluo%3D';
+  const url = 'http://64.225.99.144/vascin-translation-files/DD0974DF-6854-9677-74C8-5CB1C30AEB90_ar_transfile.srt?response-cache-control=max-age%3D86400&AWSAccessKeyId=PSFBSAZRKNBJOAMKHHBIBOBEONKBBOPKEDDBFBOJCH&Expires=1783958715&Signature=wSHpLyQbtuG31Nkd8u0wqKGEluo%3D';
   try {
     const res = await fetch(url, {
       headers: {
