c244b7fc434f991e245e710c22fa40c7ab0b6558
Gaoc3 <secon2636@gmail.com>
2026-06-13 19:25:07 +0300

    Auto-commit: 2026-06-13 19:25:07

--- check_sub.js
+++ check_sub.js
@@ -1,10 +1,9 @@
 async function test() {
-  const url = 'http://64.225.99.144/vascin-translation-files/DD0974DF-6854-9677-74C8-5CB1C30AEB90_ar_transfile.srt?response-cache-control=max-age%3D86400&AWSAccessKeyId=PSFBSAZRKNBJOAMKHHBIBOBEONKBBOPKEDDBFBOJCH&Expires=1783958715&Signature=wSHpLyQbtuG31Nkd8u0wqKGEluo%3D';
+  const url = 'http://cnth2.shabakaty.com/vascin-translation-files/DD0974DF-6854-9677-74C8-5CB1C30AEB90_ar_transfile.srt?response-cache-control=max-age%3D86400&AWSAccessKeyId=PSFBSAZRKNBJOAMKHHBIBOBEONKBBOPKEDDBFBOJCH&Expires=1783958715&Signature=wSHpLyQbtuG31Nkd8u0wqKGEluo%3D';
   try {
     const res = await fetch(url, {
       headers: {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
-        'Bypass-Tunnel-Reminder': 'true',
         'Referer': 'https://cinemana.shabakaty.com/',
       }
     });
